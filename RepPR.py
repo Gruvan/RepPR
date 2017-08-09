b61eb090f9f5c430f8450232b233e1a15c1504e1
Adam Bergman <adambergman@live.se>
2017-08-09 17:21:46 +0200

    Början till att fixa interaktionen med databasen och rekorden

--- RepPR.py
+++ RepPR.py
@@ -11,23 +11,26 @@ from kivy.adapters.listadapter import ListAdapter
 from kivy.properties import ObjectProperty
 from kivy.uix.textinput import TextInput
 
+from kivy.uix.spinner import Spinner
 
-lista = [{"weight": 90, "reps": 3, "est1RM": 100},
-         {"weight": 81, "reps": 4, "est1RM": 95},
-         {"weight": 72, "reps": 5, "est1RM": 90},
-         {"weight": 71, "reps": 6, "est1RM": 80},
-         {"weight": 70, "reps": 6, "est1RM": 80},
-         {"weight": 73, "reps": 6, "est1RM": 80},
-         {"weight": 12, "reps": 6, "est1RM": 80},
-         {"weight": 23, "reps": 6, "est1RM": 80},
-         {"weight": 12, "reps": 6, "est1RM": 80}]
+from liftCalc import *
+
+# lista = [{"weight": 90, "reps": 3, "est1RM": 100},
+#          {"weight": 81, "reps": 4, "est1RM": 95},
+#          {"weight": 72, "reps": 5, "est1RM": 90},
+#          {"weight": 71, "reps": 6, "est1RM": 80},
+#          {"weight": 70, "reps": 6, "est1RM": 80},
+#          {"weight": 73, "reps": 6, "est1RM": 80},
+#          {"weight": 12, "reps": 6, "est1RM": 80},
+#          {"weight": 23, "reps": 6, "est1RM": 80},
+#          {"weight": 12, "reps": 6, "est1RM": 80}]
 
 
 def prettyOutprint(dicts):
     for row in dicts:
-        row["text"] = str(row["est1RM"]) \
+        row["text"] = str(row["weight"]) \
             + " kg x " + str(row["reps"]) \
-            + " reps"
+            + " reps ---> " + str(row['est1RM']) + " kg"
     return dicts
 
 
@@ -35,6 +38,8 @@ def my_callback():
     print("hej")
 
 
+lista = EpleySort(lista)
+lista = filterDuplicates(lista)
 prettyOutprint(lista)
 
 
--- liftCalc.py
+++ liftCalc.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from operator import itemgetter
 from collections import Counter
 import sqlite3 as sql
@@ -8,36 +10,99 @@ lista = [{"weight": 10, "reps": 3},
          {"weight": 40, "reps": 6},
          {"weight": 50, "reps": 4},
          {"weight": 33, "reps": 6},
-         {"weight": 12, "reps": 6},
+         {"weight": 12, "reps": 7},
          {"weight": 100, "reps": 9},
-         {"weight": 133, "reps": 7}]
+         {"weight": 105, "reps": 1},
+         {"weight": 125, "reps": 2},
+         {"weight": 120, "reps": 11},
+         {"weight": 120, "reps": 12},
+         {"weight": 120, "reps": 13},
+         {"weight": 120, "reps": 14}]
 
 
-conn = sql.connect("records.db")
-cur = conn.cursor()
-
-
-def initLiftData():
+def getAll():
     conn = sql.connect("records.db")
     cur = conn.cursor()
-    cur.execute('SELECT * FROM Bench')
+    cur.execute('SELECT * FROM lifts')
     for row in cur.fetchall():
         print(row)
 
+def initLiftData(liftName):
+    conn = sql.connect("records.db")
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM lifts WHERE lift == ?;', [liftName])
+    liftsTable = [dict(liftName=row[0],
+                       weight=row[1],
+                       reps=row[2],
+                       estmax=row[3]) for row in cur.fetchall()]
+    conn.close()
+    return liftsTable
 
-def addEntry(weight, reps):
-    # if reps > 10:
-    # break
-    pass
 
+def addEntry(liftName, weight, reps):
+    conn = sql.connect("records.db")
+    cur = conn.cursor()
+    cur.execute("""INSERT OR REPLACE INTO lifts (lift, weight, reps)
+                   VALUES (?, ?, ?);""", [liftName, weight, reps])
+    conn.commit()
+    conn.close()
 
-def EpleyAndSort(dic):
+def newLift(liftName):
+    conn = sql.connect("records.db")
+    cur = conn.cursor()
+    lista = []
+    for reps in range(0,11):
+        #dicce = {"liftname": liftName, "weight:":0, "reps": i, "estmax": 0}
+        #lista.append(dicce)
+        cur.execute('INSERT into lifts (lift, weight, reps) VALUES (?, ?, ?);', [liftName, 0, reps])
+    conn.commit()
+    conn.close()
+
+        
+def replace(liftName,weight, reps):
     def innerEpley(weight, reps):
-        max = weight * (1 + reps / float(30))
-        return max
+        if reps == 1:
+            return weight
+        else:
+            return weight * (1 + reps / float(30))
+    estmax = innerEpley(weight,reps)
+
+    conn = sql.connect("records.db")
+    cur = conn.cursor()
+    cur.execute("""UPDATE lifts 
+                   SET weight == ?, estmax == ?
+                   WHERE reps == ? AND lift == ?;""", [weight, estmax, reps, liftName])
+    #cur.execute('INSERT OR REPLACE INTO lifts (lift, weight, reps) VALUES (?, ?, ?);', [liftName, weight, reps])
+    
+
+    [print(row) for row in cur.fetchall()]
+    conn.commit()
+    conn.close()
+
+
+def EpleySort(dic):
+    def innerEpley(weight, reps):
+        if reps == 1:
+            return weight
+        else:
+            return weight * (1 + reps / float(30))
+
     for row in dic:
         row["est1RM"] = innerEpley(row['weight'], row['reps'])
     sorted_list = sorted(dic, key=itemgetter('est1RM'), reverse=True)
+    return sorted_list
+
+
+def normalSort(dic):
+    def innerEpley(weight, reps):
+        if reps == 1:
+            return weight
+        else:
+            return weight * (1 + reps / float(30))
+
+    for row in dic:
+        row["est1RM"] = innerEpley(row['weight'], row['reps'])
+    sorted_list = sorted(dic, key=itemgetter('est1RM'), reverse=False)
     return sorted_list
 
 
@@ -48,4 +113,4 @@ def filterDuplicates(dic):
         all = [x for x in dic if x['reps'] == i]
         new_vals.append(max(all, key=lambda x: x['est1RM']))
     sorted_list = sorted(new_vals, key=itemgetter('est1RM'), reverse=True)
-    return sorted_list
+    return sorted_list  # [:9]  # ger bara tillbaka rep 1-9
